5c8b297cf950a2632ea778c686cff41bf77017fd
f0o <f0o@devilcode.org>
2014-05-11 09:50:55 +0200

    Added helper

--- src/main.cpp
+++ src/main.cpp
@@ -1451,6 +1451,19 @@ unsigned int static GetNextWorkRequired(const CBlockIndex* pindexLast, const CBl
 	
 }
 
+double ConvertBitsToDouble(unsigned int nBits){
+	int nShift = (nBits >> 24) & 0xff;
+	double dDiff = (double)0x0000ffff / (double)(nBits & 0x00ffffff);
+	while (nShift < 29) {
+		dDiff *= 256.0;
+		nShift++;
+	}
+	while (nShift > 29) {
+		dDiff /= 256.0;
+		nShift--;
+	}
+	return dDiff;
+}
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits) {
 	CBigNum bnTarget;
